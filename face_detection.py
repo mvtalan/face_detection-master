3902cb18e0b32dfdad131baf4c3e2a9f7c06312d
mvtalan <mvtalan@gmail.com>
2024-10-10 15:08:39 -0600

    working codes

--- face_detection.py
+++ face_detection.py
@@ -18,7 +18,7 @@ known_face_names = [
     'Mark'
 ]
 
-print(known_face_encodings)
+# print(known_face_encodings)
 
 face_locations = []
 face_encodings = []
@@ -32,10 +32,64 @@ while True:
     #only process every other frame
     if process_this_frame:
         #resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0,0), fx=0.25, y=0.25)
+        small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
         
         #convert image from BGR (OpenCV) to RGB (face_recognition uses)
         rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
         
         #find all faces and facial encodings in the current frame of the video
         face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        
+        face_names = []
+        for face_encoding in face_encodings:
+            #see if face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            
+            name = "unknown"
+            
+            #if match found, just use the first one
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+                
+            #or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+    
+    #display the results
+    for(top, right, bottom, left), nae in zip(face_locations, face_names):
+        #scale back up face location
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+        
+        #draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        
+        #draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        
+        # if name != "Unknown":
+        #     cv2.putText(frame, "Match.", (10, 30), font, 1.0 (0, 255, 0), 2, cv2.LINE_AA)
+        
+    #display the resulting image
+    cv2.imshow('Video', frame)
+    
+    #hit 'q' on keyboard to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+#release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+        
